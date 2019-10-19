8234538a70c6bc3f6c220465aa6891aacaa91a25
andreas@andreas-hahn.at <andreas@andreas-hahn.at>
2019-10-19 13:28:46 +0200

    added console information

--- client/src/index.js
+++ client/src/index.js
@@ -25,6 +25,20 @@ import {feedMiddleware} from "./game/Feed/feed.middleware";
 
 export const history = createBrowserHistory();
 
+console.log("%cOwe Drahn", "font-size:40px; color:#7289da;");
+
+const consoleAnimation = [
+    "background-image: url(\"http://bestanimations.com/Games/Dice/rolling-dice-gif-7.gif\")",
+    "background-size: cover",
+    "padding: 30px 50px",
+    "line-height: 50px"
+].join(";");
+console.log("%c ", consoleAnimation);
+const consoleInformation = [
+    "color: #7289da"
+].join(";");
+console.log("%cIf you want to contribute check out the repo https://github.com/drdreo/Owe-Drahn ", consoleInformation);
+
 if (prod) {
     initGa(history);
 }
