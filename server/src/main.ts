4ffea57b8dd73a321b7d7a4339e9a10d258ab44a
Dr Dreo <drdreo@users.noreply.github.com>
2022-12-13 19:47:54 +0100

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,18 +3,28 @@ import { AppModule } from './app.module';
 import * as session from 'express-session';
 import { EnvironmentService } from './environment.service';
 
-const whitelistDomains = [
+const allowlistDomains = [
     'http://localhost:3000',
     'http://localhost:4000',
     'http://owe-drahn.pages.dev/',
     'https://owe-drahn.pages.dev/'
 ];
 
+const corsOptionsDelegate = function (req, callback) {
+  const corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
         credentials: true,
-        origin: whitelistDomains
+        origin: corsOptionsDelegate
     });
 
     app.use(session({
