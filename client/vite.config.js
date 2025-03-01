657790130184b45a59eb987dc59bf96048a7c757
DrDreo <code@drdreo.com>
2025-03-01 12:05:37 +0100

    add sentry wizard

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
+import {sentryVitePlugin} from "@sentry/vite-plugin";
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        sentryVitePlugin({
+            org: "drdreo",
+            project: "owe-drahn"
+        })
+    ],
+    build: {
+        sourcemap: true
+    }
 })
